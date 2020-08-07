e27f42858bc255fa382380a3d6450c7eead4e887
kin3303 <48729027+kin3303@users.noreply.github.com>
2020-08-07 16:43:33 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World Docker!"
+    return "Hello World Docker2!"
 
 if __name__ == "__main__":
     application.run()
